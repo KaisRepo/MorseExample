f622deddd0083c6e3e1a2414835c1ab6a9405ae3
Splapp <kai.pehrsson@gmail.com>
2016-08-12 21:09:12 -0700

    fixed morse.js, to show example of how to use script if no arguments are given.

--- morse.js
+++ morse.js
@@ -1,4 +1,11 @@
 var morse = require('morse-node').create();
+var plain_text = "";
+if(process.argv.length > 2) {
+    plain_text = process.argv[2];
+} else {
+  console.log("\nEnter string to be encoded.\n\nExample:\n\tnodejs morse.js \"hack the planet\"\n");
+  return;
+}
 
 console.log("enter a message to be converted into morse: " + process.argv[2]);
 var encodedmsg = morse.encode(process.argv[2]);
